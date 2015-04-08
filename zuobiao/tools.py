11149b95e2286673de14c20cef4d09c83c549000
linliguang <linliguang@demlution.com>
2015-04-08 14:49:47 +0800

    Use objects.bulk_create.

--- zuobiao/tools.py
+++ zuobiao/tools.py
@@ -11,7 +11,7 @@ from dashboard.models import Question, Anser, User
 from dashboard.models import SEX_CHOICES, INCOME_CHOICES, EDU_CHOICES, ANSER_CHOICES
 
 import datetime
-import re 
+import re
 
 # Import date from csv
 with open('2014data.csv', 'r') as f:
@@ -21,7 +21,7 @@ with open('2014data.csv', 'r') as f:
         print(i)
         if i == 0:
             for item in row[3:-4]:
-                question, is_question_create = Question.objects.get_or_create(name=item.strip())
+                question = Question.objects.create(name=item.strip())
                 questions.append(question)
         else:
             created, ip, = row[1:3]
@@ -32,9 +32,9 @@ with open('2014data.csv', 'r') as f:
             created = datetime.datetime.strptime(created, '%Y-%m-%d %H:%M:%S')
             education = [k for k, v in EDU_CHOICES if education == v][0]
             income = [k for k, v in INCOME_CHOICES if income == v][0]
-            sex = 1 if sex == 'M' else 2 
-            
-            user, is_user_create = User.objects.get_or_create(
+            sex = 1 if sex == 'M' else 2
+
+            user = User.objects.create(
                 created=created,
                 education=education,
                 income=income,
@@ -43,19 +43,12 @@ with open('2014data.csv', 'r') as f:
                 birthday=birthday
             )
 
-            error = False 
-            for item in row[3:-4]:
-                if item not in [v for k, v in ANSER_CHOICES]:
-                    error = True 
-
-            if error:
-                print(error)
-
-            if not error and user:
-                for index, item in enumerate(row[3:-4]):
-                    Anser.objects.get_or_create(
-                        created=created,
-                        question=questions[index],
-                        user=user,
-                        choice=[k for k, v in ANSER_CHOICES if item == v][0]
-                    )
+            ansers = []
+            for index, item in enumerate(row[3:-4]):
+                ansers.append(Anser(
+                    created=created,
+                    question=questions[index],
+                    user=user,
+                    choice=[k for k, v in ANSER_CHOICES if item == v][0]
+                ))
+            Anser.objects.bulk_create(ansers)
