5ab0202d2f872d420f0875f7654f499ffd4d4894
phyng <phyngk@gmail.com>
2015-04-09 19:29:45 +0800

    Update for pypy.

--- zuobiao/zuobiao/settings.py
+++ zuobiao/zuobiao/settings.py
@@ -8,6 +8,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+try:
+    import psycopg2
+except ImportError:
+    from psycopg2cffi import compat
+    compat.register()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
